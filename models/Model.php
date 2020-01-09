497a9f678c0ac277a591b37228bfbb3b7c036337
bruno <egremy.bruno@gmail.com>
2020-01-09 09:43:16 +0100

    Gestion d'un .ini, dans la classe abstraite Model, pour les infos de connexion à la base de donnée. Le fichier dbConfig.ini doit etre placé à la racine du projet.

--- models/Model.php
+++ models/Model.php
@@ -1,12 +1,31 @@
 <?php
+
 // modèle pour les classes d'objets représentant un Jeu, un Achat, etc.
 abstract class Model {
 	
 	private static $_db;
-	
+	private static $_host;
+	private static $_dbName;
+	private static $_user;
+	private static $_passwd;
+
+    // constructeur préparant les paramètre de connexion à la BD
+    public function __construct() {
+        $config = parse_ini_file('../dbConfig.ini');
+
+        self::$_host = $config['db_host'];
+        self::$_dbName = $config['db_name'];
+        self::$_user = $config['db_user'];
+        self::$_passwd = $config['db_password'];
+    }
+
+    private static function getDBConf(){
+
+    }
+
 	// instancie la connexion à la base de données
 	private static function setDB() {
-		self::$_db = new PDO('mysql:host=localhost;dbname=stome;charset=utf8', 'root', 'root');
+		self::$_db = new PDO('mysql:host=' . self::$_host . ';dbname=' . self::$_dbName . ';charset=utf8', self::$_user, self::$_passwd);
 		self::$_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 	}
 	
