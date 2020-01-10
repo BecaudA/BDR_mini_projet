d0c77b60989c26b1ff7f426ab178400e8cc4b317
bruno <egremy.bruno@gmail.com>
2020-01-10 15:31:02 +0100

    Centralisation des requetes

--- models/CompteManager.php
+++ models/CompteManager.php
@@ -2,6 +2,7 @@
 // le <class>Manager fait la requête vers la base de données
 class CompteManager extends Model {
 	public function getComptes() {
-		return $this->reqSelectDB('SELECT * FROM stome.Compte', 'Compte');
+        $request = parse_ini_file('requests.ini')['getAllAccounts'];
+		return $this->reqSelectDB($request, 'Compte');
 	}
 }
