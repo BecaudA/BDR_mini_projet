fd676c0c566932587f981f9d7a36c652fd644c03
bruno <egremy.bruno@gmail.com>
2020-01-18 14:33:49 +0100

    Working

---
+++ views/viewBoutique.php
@@ -0,0 +1,25 @@
+<div class="container">
+
+    <h1 class="font-weight-light text-center text-lg-left mt-4 mb-0">Boutique</h1>
+    <hr class="mt-2 mb-5">
+    <div class="row text-center text-lg-left">
+
+        <?php
+        foreach ($produits as $produit){
+
+            $titre = $produit->titre();
+
+            if($titre != null) {
+                // Affiche une image de forme "titre.jpg" le nom de l'image doit correspondre au Titre du produit mais sans espace et en minuscule
+                echo "  <div class=\"col-lg-3 col-md-4 col-6\">
+                        <a href=\"" . $titre . "\" class=\"d-block mb-4 h-100\">
+                            <img class=\"img-fluid img-thumbnail\" src=\"../img/thumbnails/" . strtolower(str_replace(' ', '', $titre)) . ".jpg\" alt=\"\"></a>
+                    </div>";
+            }
+        }
+        ?>
+    </div>
+</div>
+
+
+
