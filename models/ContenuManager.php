d9de2657b4cc208ef15cff2747648de452830a19
Naludrag <stephanecarvalho81@gmail.com>
2020-01-18 16:53:27 +0100

    Contenu

--- models/ContenuManager.php
+++ models/ContenuManager.php
@@ -1,11 +1,8 @@
 <?php
-
-include 'requests.php';
-
 class ContenuManager extends Model {
 
     public function getDetailsContenu($titre) {
-        $contenu = $this->reqSelectDB(getContenu("Monster Hunter World"), "Contenu");
+        $contenu = $this->reqSelectDB(getContenu($titre), "Contenu");
 
         $data = $this->reqSelectDB_Tuples(getLanguesContenu($contenu[0]->titre()));
         $contenu[0]->set_langues($data);
