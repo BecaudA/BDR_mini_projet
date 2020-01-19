8651640da780af3ba763c806651bfec45e556129
Naludrag <stephanecarvalho81@gmail.com>
2020-01-19 17:25:25 +0100

    Ajout de la gestion d'un Bundle

--- models/Bundle.php
+++ models/Bundle.php
@@ -2,12 +2,30 @@
 
 
 class Bundle extends Produit {
-    private $_listProduit;
+    private $_listProduit = array();
 
     // constructeur
     public function __construct(array $data) {
         parent::__construct($data);
-        // get la liste des produits du bundle
-        // $this->_listProduit = (new ProduitManager()).get
+        $this->hydrate($data);
+    }
+
+    // hydratation (initialise les champs de cet objet)
+    public function hydrate(array $data) {
+        foreach ($data as $key => $value) {
+            $method = 'set_'.ucfirst($key);
+
+            if (method_exists($this, $method)) {
+                $this->$method($value);
+            }
+        }
+    }
+
+    public function set_listeProduits(array $data){
+        $this->_listProduit = $data;
+    }
+
+    public function listeProduits(){
+        return $this->_listProduit;
     }
 }
